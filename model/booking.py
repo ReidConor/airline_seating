caaa1a97a9577413f64acfc966bb08aa0ae82371
Conor Reid <conor.reid@ucdconnect.ie>
2017-02-20 22:14:19 +0000

    project clean up

--- model/booking.py
+++ model/booking.py
@@ -1,12 +1,9 @@
 import sys
 
-
 class Booking:
     def __init__(self, name, parties):
         self.name = name
         self.parties = parties
-        self.accepted = False
-        self.sittingTogether = False
 
     def __str__(self):
-        return self.name + " " + self.parties
+        return self.name + " " + str(self.parties)
