b298fe06f5691bc24e5ef4fdf57ffa7a748a848b
Conor Reid <conor.reid@ucdconnect.ie>
2017-02-21 20:01:20 +0000

    WIP - adding testing framework

---
+++ test/unitTest.py
@@ -0,0 +1,14 @@
+import unittest
+
+#from https://docs.python.org/2/library/unittest.html
+class airlineReservationSystemTests(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
